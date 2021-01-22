e9f4e984092ad351b23c72809530a5b06d56edea
Tran Quang Vinh <qvr@elca.vn>
2021-01-22 14:58:45 +0700

    Review and update

--- dotnet/04-dotnet-Nhibernate/NHibernateExercises/NHibernateExercises/Program.cs
+++ dotnet/04-dotnet-Nhibernate/NHibernateExercises/NHibernateExercises/Program.cs
@@ -31,76 +31,80 @@ namespace NHibernateExercises
             var parameterService = container.GetInstance<IParameterService>();
             var numbers = projectService.GetAllProjectNumbers().Distinct().ToList();
 
-            //// Exercise 1: Mapping with chapter 8 - Session & unit of work advance
-            //// why the transaction deadlock occurs below with ProcessProjectInParallel ? How will we solve it ?
-            //Console.WriteLine("Exercise 1: why the transaction deadlock occurs below with ProcessProjectInParallel ? How will we solve it ?");
-            //projectService.ProcessProjectInParallel(numbers);
-            //Console.WriteLine("------------------------------------------------------------------------------------------------------------");
+            // Exercise 1: Mapping with topic #1 - Session & Unit of work
+            // please verify the object return by method BaseRepository.Load(int id) and BaseRepository.GetById(int id)
+            // What's different between them ?
 
-            //// Exercise 2 : Mapping with chapter 8 - Concurrent update
-            //// why OptimisticVersionException is thrown while there is no row into DB for table ParameterDefinition + ParameterValue ?
-            //Console.WriteLine("Exercise 2: why OptimisticVersionException is thrown while there is no row into DB for table ParameterDefinition + ParameterValue ?");
-            //try
-            //{
-            //    parameterService.DeleteAllParameters();
-            //    var parameter = new ParameterDefinitionEntity
-            //    {
-            //        Id = 1,
-            //        Name = $"Parameter1",
-            //        Type = "Integer",
-            //        RowVersion = 1
-            //    };
-            //    var parameterValue = new ParameterValueEntity
-            //    {
-            //        Id = 1,
-            //        Value = "1",
-            //        Year = DateTime.Now.Year,
-            //        ParameterDefinition = parameter,
-            //        RowVersion = 1
-            //    };
-            //    parameter.ParameterValues.Add(parameterValue);
-            //    parameterService.InsertParameter(parameter);
-            //    Console.WriteLine("Insert parameter successful. You passed Exercise 2 !");
-            //}
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine("Error to process for number : " + ex);
-            //}
-            //Console.WriteLine("------------------------------------------------------------------------------------------------------------");
+            // Exercise 2: Mapping with topic #1 - Session & Unit of work
+            // why the transaction deadlock occurs below with ProcessProjectInParallel ? How will we solve it ?
+            Console.WriteLine("Exercise 1: why the transaction deadlock occurs below with ProcessProjectInParallel ? How will we solve it ?");
+            projectService.ProcessProjectInParallel(numbers);
+            Console.WriteLine("------------------------------------------------------------------------------------------------------------");
 
-            //// Assume : you solved the Exercise 3 and saved the parameter into DB successful 
-            //// Exercise 3 : Mapping with chapter 6 - Cascade/Inverse  
-            //// Why ParameterValue is not deleted into DB when we clear the list 'ParameterValues' and save into DB ? How do we delete ParameterValue by using Cascade? 
-            //Console.WriteLine("Exercise 3: (Cascade)  Why ParameterValue is not deleted into DB when we clear the list 'ParameterValues' and save into DB ? How do we delete ParameterValue by using Cascade? ");
-            //var para = parameterService.LoadParameter(1);
-            //para.ParameterValues.Clear();
-            //parameterService.UpdateParameter(para);
-            //para = parameterService.LoadParameter(1);
-            //if (para.ParameterValues.Count == 0)
-            //{
-            //    Console.WriteLine("Delete ParameterValue successful. You passed Exercise 3 !");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("ParameterValue is not deleted into DB");
-            //}
-            //Console.WriteLine("------------------------------------------------------------------------------------------------------------");
+            // Exercise 3 : Mapping with topic #2 - Concurrent update
+            // why OptimisticVersionException is thrown while there is no row into DB for table ParameterDefinition + ParameterValue ?
+            Console.WriteLine("Exercise 2: why OptimisticVersionException is thrown while there is no row into DB for table ParameterDefinition + ParameterValue ?");
+            try
+            {
+                parameterService.DeleteAllParameters();
+                var parameter = new ParameterDefinitionEntity
+                {
+                    Id = 1,
+                    Name = $"Parameter1",
+                    Type = "Integer",
+                    RowVersion = 1
+                };
+                var parameterValue = new ParameterValueEntity
+                {
+                    Id = 1,
+                    Value = "1",
+                    Year = DateTime.Now.Year,
+                    ParameterDefinition = parameter,
+                    RowVersion = 1
+                };
+                parameter.ParameterValues.Add(parameterValue);
+                parameterService.InsertParameter(parameter);
+                Console.WriteLine("Insert parameter successful. You passed Exercise 2 !");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error to process for number : " + ex);
+            }
+            Console.WriteLine("------------------------------------------------------------------------------------------------------------");
 
-            //// Exercise 4: Mapping with chapter 9 - Batch update/delete/insert + chapter 1 - Performance Optimizations
-            //// Please improve the performance for two methods CreateAllParameters & DeleteAllParameters 
-            //Console.WriteLine("Exercise 4: Please improve the performance for two methods CreateAllParameters & DeleteAllParameters");
-            //Stopwatch stopWatch = new Stopwatch();
-            //stopWatch.Start();
-            //parameterService.CreateAllParameters();
-            //stopWatch.Stop();
-            //Console.WriteLine($"Create all parameters in {stopWatch.Elapsed.TotalSeconds} seconds");
-            //stopWatch.Start();
-            //parameterService.DeleteAllParameters();
-            //stopWatch.Stop();
-            //Console.WriteLine($"Delete all parameters in {stopWatch.Elapsed.TotalSeconds} seconds");
-            //Console.WriteLine("------------------------------------------------------------------------------------------------------------");
+            // Exercise 4 : Mapping with topic #3 - Cascade/Inverse  
+            // Assume : you solved the Exercise 3 and saved the parameter into DB successful 
+            // Why ParameterValue is not deleted into DB when we clear the list 'ParameterValues' and save into DB ? How do we delete ParameterValue by using Cascade? 
+            Console.WriteLine("Exercise 3: (Cascade)  Why ParameterValue is not deleted into DB when we clear the list 'ParameterValues' and save into DB ? How do we delete ParameterValue by using Cascade? ");
+            var para = parameterService.LoadParameter(1);
+            para.ParameterValues.Clear();
+            parameterService.UpdateParameter(para);
+            para = parameterService.LoadParameter(1);
+            if (para.ParameterValues.Count == 0)
+            {
+                Console.WriteLine("Delete ParameterValue successful. You passed Exercise 3 !");
+            }
+            else
+            {
+                Console.WriteLine("ParameterValue is not deleted into DB");
+            }
+            Console.WriteLine("------------------------------------------------------------------------------------------------------------");
 
-            // Exercise 5: mapping with chapter 3 - Model inheritance (join) & chapter 2 - Advanced mapping concepts
+            // Exercise 5: Mapping with topic #4 - Performance Optimizations & Batch update/delete/insert  
+            // Please improve the performance for two methods CreateAllParameters & DeleteAllParameters 
+            Console.WriteLine("Exercise 4: Please improve the performance for two methods CreateAllParameters & DeleteAllParameters");
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            parameterService.CreateAllParameters();
+            stopWatch.Stop();
+            Console.WriteLine($"Create all parameters in {stopWatch.Elapsed.TotalSeconds} seconds");
+            stopWatch.Start();
+            parameterService.DeleteAllParameters();
+            stopWatch.Stop();
+            Console.WriteLine($"Delete all parameters in {stopWatch.Elapsed.TotalSeconds} seconds");
+            Console.WriteLine("------------------------------------------------------------------------------------------------------------");
+
+            // Exercise 6: mapping with topic #5 - Advanced mapping concepts & Model inheritance (join)
             // We have Table per class hierarchy, Table per type 
             // please extend table ParameterValue so that it can store for both OutputParameterValue and InputParameterValue
             // The OutputParameterValue will have more column FormatData nvarchar(100) , the InputParameterValue will have the column IsMandatory bit
@@ -110,18 +114,18 @@ namespace NHibernateExercises
             // For Table per type, we will have 3 table OutputParameterValue, InputParameterValue, ParameterValue
 
 
-            // Exercise 6: Mapping with chapter 4 - Interceptor / event model on NHibernate
+            // Exercise 7: Mapping with topic #6 - Interceptor / event model on NHibernate
             // implement interceptor
             // We will extend the table Project with new columns CreationDate - UpdateDate
             // Please implement the interceptor so that when we save/update data, it will automatically update for these column CreationDate - UpdateDate
 
 
-            // Exercise 7: Mapping with chapter 5 - Nhibernate Envers
-            // please write query to get all revisions of of project with id = 1
+            // Exercise 8: Mapping with topic #7 - Nhibernate Envers
+            // please write query to get all revisions of project with id = 1
             // and query to get a revisions of projects
             //var x = projectService.GetAllProjectAudit();
 
-            // Exercise 8: Mapping with chapter 10 - NHibernate validator
+            // Exercise 9: Mapping with topic #8 - NHibernate validator
             // please write a custom validator for Project which make sure ModificationDate >= CreationDate
             //ValidatorEngine validator = new ValidatorEngine();
             //var project = new ProjectEntity()
@@ -129,10 +133,6 @@ namespace NHibernateExercises
             //    Name = new string('*', 255),
             //};
             //bool isValid = validator.IsValid(project);
-
-            // Exercise 9: please verify the object return with getById and session.Load
-            // What's different between them
-
         }
     }
 }
