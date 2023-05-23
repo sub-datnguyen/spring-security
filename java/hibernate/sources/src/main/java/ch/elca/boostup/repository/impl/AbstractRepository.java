2e12140eaa6414a6be57ff67b35d56531a8d599b
tcb <canh.bui@elca.vn>
2023-05-23 19:08:11 +0700

    update version + code

--- java/hibernate/sources/src/main/java/ch/elca/boostup/repository/impl/AbstractRepository.java
+++ java/hibernate/sources/src/main/java/ch/elca/boostup/repository/impl/AbstractRepository.java
@@ -3,45 +3,20 @@ package ch.elca.boostup.repository.impl;
 import ch.elca.boostup.entity.AbstractCodeBaseEntity;
 import ch.elca.boostup.util.JpaUtil;
 import com.querydsl.core.types.EntityPath;
-import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.impl.JPAQuery;
-import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import org.apache.commons.lang3.reflect.TypeUtils;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.support.JpaEntityInformation;
-import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
-import org.springframework.data.jpa.repository.support.Querydsl;
-import org.springframework.data.querydsl.EntityPathResolver;
-import org.springframework.data.querydsl.SimpleEntityPathResolver;
+import java.util.Optional;
 
-public abstract class AbstractRepository<T extends AbstractCodeBaseEntity> implements InitializingBean {
-
-    private static final EntityPathResolver DEFAULT_ENTITY_PATH_RESOLVER = SimpleEntityPathResolver.INSTANCE;
-
+public abstract class AbstractRepository<T extends AbstractCodeBaseEntity> {
     @PersistenceContext
     private EntityManager em;
 
     @Autowired
     protected JpaUtil jpaUtil;
-
-    protected EntityPath<T> path;
-    protected PathBuilder<T> builder;
-    protected Querydsl querydsl;
-    protected JpaEntityInformation<T, String> entityInformation;
-
-    @Override
-    public void afterPropertiesSet() {
-        if (getDomainClass() != null) {
-            entityInformation = new JpaRepositoryFactory(em).getEntityInformation(getDomainClass());
-            this.path = DEFAULT_ENTITY_PATH_RESOLVER.createPath(entityInformation.getJavaType());
-            this.builder = new PathBuilder<>(path.getType(), path.getMetadata());
-            this.querydsl = new Querydsl(em, builder);
-        }
-    }
 
     protected <K extends AbstractCodeBaseEntity> JPAQuery<K> selectFrom(EntityPath<K> path) {
         return new JPAQuery<K>(em).from(path);
@@ -49,12 +24,6 @@ public abstract class AbstractRepository<T extends AbstractCodeBaseEntity> imple
 
     public Optional<T> findOne(JPAQuery<T> path) {
         return Optional.ofNullable(path.fetchOne());
-    }
-
-    protected Class<T> getDomainClass() {
-        return (Class<T>) TypeUtils.getTypeArguments(getClass(), AbstractRepository.class)
-            .values()
-            .iterator().next();
     }
 
     protected Query createNativeQuery(String queryString) {
